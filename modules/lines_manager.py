9c36a0df6f5a16294017aee6b4fe26ee785f5e2a
Zemphix <vafinaaa51@gmail.com>
2025-02-18 08:34:38 +0500

    added script for control 'linear tactic'

---
+++ modules/lines_manager.py
@@ -0,0 +1,42 @@
+from modules.stab_manager import (LinesManager, 
+                                  TempKlinesManager)
+
+
+class CrossLines:
+
+    def __init__(self):
+        self.klines = TempKlinesManager()
+        self.lines = LinesManager()
+        pass
+
+    def __get_data(self):
+        klines = self.klines.get_updated()
+        buy_lines = self.lines.get_buy_lines()
+        sell_lines = self.lines.get_sell_lines()
+        pre_pre_last_kline = float(klines[2][4])
+        pre_last_kline = float(klines[1][4])
+
+        return buy_lines, sell_lines, pre_last_kline, pre_pre_last_kline
+    
+    def cross_down_to_up(self): # Продажа
+        _, sell_lines, pre_last_kline, pre_pre_last_kline = self.__get_data()
+        
+        for sell_line in sell_lines[::-1]:
+            if pre_last_kline > sell_line: # Цена последней свечи выше линии
+                if pre_pre_last_kline < sell_line: # Цена предпоследней свечи ниже чем линия продажи
+                    if pre_last_kline > pre_pre_last_kline: # Цена последней свечи выше чем свечи до нее
+                        print(sell_line)
+                        return True
+        return False
+    
+    def cross_up_to_down(self): # Покупка
+        buy_lines, _, pre_last_kline, pre_pre_last_kline = self.__get_data()
+        
+        for buy_line in buy_lines:
+            if pre_last_kline < buy_line: # Цена последней свечи ниже линии
+                if pre_pre_last_kline > buy_line: # Цена предпоследней свечи выше чем линия продажи
+                    if pre_last_kline < pre_pre_last_kline: # свеча красная
+                        print(buy_line)
+                        return True
+        return False
+
