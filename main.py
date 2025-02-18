ea6d33f4793593e57ac39c3cde4fba0228c13f57
Zemphix <vafinaaa51@gmail.com>
2025-02-18 08:40:52 +0500

    added main file for starting

---
+++ main.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+from cfg import logger_config
+from bot.bot import Bot
+import random
+from modules.notifies_manager import TeleNotify
+import logging
+logger = logging.getLogger(__name__)
+logging.getLogger('_http_manager').setLevel(logging.WARNING)
+
+def main():
+    try: 
+        Bot().activate()
+    except Exception as e:
+        eid = random.randint(100000, 9999999)
+        TeleNotify(True).error(f"Error id: {eid}.\nReport the error to the developer, naming the error id")
+        logger.error("Error id: %s. Message: %s", eid, e)
+
+
+if __name__ == "__main__":
+    main()
