03596f3ca78526d31b1198139c21152c1e8def46
Zemphix <vafinaaa51@gmail.com>
2025-02-18 08:25:14 +0500

    code clean #1

--- bot/bot.py
+++ bot/bot.py
@@ -30,36 +30,4 @@ class Bot(BotBase):
             self.buy_script.activate()
             self.averating_script.activate()
             self.sell_script.activate()
-    # def start(self) -> None:
-    #     while True:
-    #             time.sleep(3)
-    #             self.profit_edit.add_profit()
-    #             balance = self.account.get_balance()
-
-    #             #Первая покупка
-    #             if self.orders.qty() == 0:
-    #                 logger.debug('trying "first buy"')
-    #                 if float(balance.get('USDT')) > self.amount_buy:
-    #                     logger.debug('activating first buy script')
-    #                     self.buy_script.activate()
-
-    #             #Усреднение
-    #             if self.orders.qty() != 0:
-    #                 logger.debug('trying "averaging"')
-    #                 if float(balance.get('USDT')) > self.amount_buy: 
-    #                     logger.debug('activating averaging script')
-    #                     self.averating_script.activate()
-
-    #             if self.lines.sell_lines_qty() > 0:
-    #                 logger.debug('sell lines qty > 0. Activating sell script')
-    #                 if self.sell_script.activate():
-    #                     self.sell_notify()
-
-    #             if self.orders.qty() != 0:
-    #                 if self.nem_notify_status == False:
-    #                     if(float(balance.get('USDT')) < self.amount_buy):
-    #                         self.notifies_edit.not_enough_money_notify()
-
-
-
-bot = Bot()
+    
